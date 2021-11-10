3f3ec13941ea0edac449812485d0c16f9d972d86
ZeroJChang <jdeleonchang@gmail.com>
2021-11-09 20:09:53 -0600

    Preparacion chats entre usuarios

---
+++ API/Models/ChatDbSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace API.Models
+{
+    public class ChatDbSettings : IChatDbSettings
+    { 
+        public string ChatCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IChatDbSettings
+    {
+        string ChatCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
