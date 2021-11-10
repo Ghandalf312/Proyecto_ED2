e3fc16d3ac01532c9ee061f0db6ff93f0d9c2ae8
ZeroJChang <jdeleonchang@gmail.com>
2021-11-09 20:37:58 -0600

    Empezando el diseño del chat

---
+++ ChatDesign/Controllers/ChattingController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using API.Models;
+using ChatDesign.Helpers;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http;
+
+namespace ChatDesign.Controllers
+{
+    public class ChattingController : Controller
+    {
+        public ActionResult Index()
+        {
+            var listOfUsers = GetUsers().Where(user => user.Username != HttpContext.Session.GetString("CurrentUser"));
+            return View(listOfUsers);
+            
+        }
+
+        private List<User> GetUsers()
+        {
+            try
+            {
+                var users = new List<User>();
+                var response = Singleton.Instance().APIClient.GetAsync("User").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    return JsonConvert.DeserializeObject<List<User>>(response.Content.ReadAsStringAsync().Result);
+                }
+                return new List<User>();
+            }
+            catch
+            {
+                return new List<User>();
+            }
+        }
+    }
+}
